aec1a220dc6074f4495ca651d5744336cddfd4e8
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2018-11-08 22:17:22 +0000

    Autoload some classes

--- v2/index.php
+++ v2/index.php
@@ -6,7 +6,14 @@
 
 use JPI\API;
 
-// Include initialisation file to include all files needed for API operations
-include 'inc/init.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/config.php';  // Copy config-sample.php and rename to config.php then fill in necessary constant
 
 API\Router::performRequest();
+
+// autoload function
+function __autoload($class) {
+	$parts = explode('\\', $class);
+	$filename = end($parts);
+	$file = "inc/$filename.php";
+	require_once($file);
+}
