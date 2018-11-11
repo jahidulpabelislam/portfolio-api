c6a15da212e161f298b3d52051d6d6ef4da6da86
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2018-11-11 00:03:43 +0000

    Restructure the classes locations

--- v3/index.php
+++ v3/index.php
@@ -4,14 +4,24 @@
  * @author Jahidul Pabel Islam
 */
 
-use JPI\API;
+use JPI\API\Router;
 
-API\Router::performRequest();
+Router::performRequest();
 
 // autoload function
 function __autoload($class) {
-	$parts = explode('\\', $class);
-	$filename = end($parts);
-	$file = "inc/$filename.php";
+
+	// The string(s) in the namespace that aren't real folder(s)
+	$namespaceNotInFile = "JPI\API\\";
+
+	// Remove any strings that are in the namespace but isn't actually a folder
+	$folders = str_replace($namespaceNotInFile, '', $class);
+	
+	// Replace the namespaces back slashes into forward slashes for folder/file paths
+	$folders = str_replace('\\', '/', $folders);
+
+	// Create the full file path to the class file
+	$file = "classes/$folders.php";
+
 	require_once($file);
 }
