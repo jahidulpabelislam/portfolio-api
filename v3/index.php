08a6b212fd821b8b1db2367400d95d0c5505aa08
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2018-12-12 20:16:15 +0000

    Use composers autoloader for my custom classes

--- v3/index.php
+++ v3/index.php
@@ -10,24 +10,8 @@
  * @copyright 2014-2018 JPI
 */
 
+require_once($_SERVER["DOCUMENT_ROOT"] .  "/vendor/autoload.php");
+
 use JPI\API\Router;
 
 Router::performRequest();
-
-// autoload function
-function __autoload($class) {
-
-	// The string(s) in the namespace that aren't real folder(s)
-	$namespaceNotInFile = "JPI\API\\";
-
-	// Remove any strings that are in the namespace but isn't actually a folder
-	$folders = str_replace($namespaceNotInFile, '', $class);
-
-	// Replace the namespaces back slashes into forward slashes for folder/file paths
-	$folders = str_replace('\\', '/', $folders);
-
-	// Create the full file path to the class file
-	$file = "classes/$folders.php";
-
-	require_once($file);
-}
