25a95217018649c64f749214d1b82821d5d826b5
Jahidul Islam <jahidul.islam@brightminded.com>
2019-04-25 13:48:03 +0100

    Update the use of ' within MySQL migrations

--- migrations/2019-04-13-144000-add-status-column-to-projects-table.sql
+++ migrations/2019-04-13-144000-add-status-column-to-projects-table.sql
@@ -1,8 +1,8 @@
 -- Add the new status column
-ALTER TABLE 'portfolio_project'
-    ADD COLUMN 'status' varchar(20) DEFAULT 'draft'
-    AFTER 'date';
+ALTER TABLE `portfolio_project`
+    ADD COLUMN `status` varchar(20) DEFAULT 'draft'
+    AFTER `date`;
 
 -- Update all status values to 'published'
-UPDATE 'portfolio_project'
-    SET 'status' = 'published';
+UPDATE `portfolio_project`
+    SET `status` = 'published';
