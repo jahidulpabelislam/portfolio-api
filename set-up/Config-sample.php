bbc545eb45d4a87d35815d3c481459950c8f67e9
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2019-10-16 22:49:43 +0100

    Rename IP to HOST

--- set-up/Config-sample.php
+++ set-up/Config-sample.php
@@ -21,8 +21,8 @@ class Config {
 
     public const API_VERSION = "3";
 
-    // IP of database server
-    public const DB_IP = "127.0.0.1"; // TODO: CHANGE ME
+    // The Host/IP of database server
+    public const DB_HOST = "127.0.0.1"; // TODO: CHANGE ME
     // Database name to use in server
     public const DB_NAME = "jpi"; // TODO: CHANGE ME
     // Username to database
