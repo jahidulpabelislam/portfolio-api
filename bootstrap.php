6de14ec7597009e1f81bc685d1ab0f8d0a3e8e7e
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2019-12-28 16:07:34 +0000

    Move global constants set up to config file

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,4 @@
 <?php
 
-if (!defined("ROOT")) {
-    define("ROOT", __DIR__);
-}
-
-require_once(ROOT . "/vendor/autoload.php");
+require_once(__DIR__ . "/config.php");
+require_once(__DIR__ . "/vendor/autoload.php");
---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+if (!defined("ROOT")) {
+    define("ROOT", __DIR__);
+}
