a7b9205966ec1aca756586cdac42c96419a70705
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2020-07-02 01:42:54 +0100

    Add ArrayAccess to Collection(s)

--- src/Collection.php
+++ src/Collection.php
@@ -2,11 +2,12 @@
 
 namespace App;
 
+use ArrayAccess;
 use ArrayIterator;
 use Countable;
 use IteratorAggregate;
 
-class Collection implements Countable, IteratorAggregate {
+class Collection implements ArrayAccess, Countable, IteratorAggregate {
 
     protected $items;
     protected $count;
@@ -20,17 +21,52 @@ class Collection implements Countable, IteratorAggregate {
         return $this->items;
     }
 
-    /**
-     * IteratorAggregate
-     */
+    protected function doesItemExists($key) {
+        return array_key_exists($key, $this->items);
+    }
+
+    protected function setItem($key, $item) {
+        if ($key === null) {
+            $this->items[] = $item;
+        }
+        else {
+            $this->items[$key] = $item;
+        }
+    }
+
+    protected function getItem($key) {
+        return $this->items[$key];
+    }
+
+    protected function removeItem($key) {
+        unset($this->items[$key]);
+    }
+
+    // ArrayAccess //
+
+    public function offsetExists($offset): bool {
+        return $this->doesItemExists($offset);
+    }
+
+    public function offsetGet($offset) {
+        return $this->getItem($offset);
+    }
+
+    public function offsetSet($offset, $item) {
+        $this->setItem($offset, $item);
+    }
+
+    public function offsetUnset($offset) {
+        $this->removeItem($offset);
+    }
+
+    // IteratorAggregate //
 
     public function getIterator(): ArrayIterator {
         return new ArrayIterator($this->items);
     }
 
-    /**
-     * Countable
-     */
+    // Countable //
 
     public function count(): int {
         return $this->count;
