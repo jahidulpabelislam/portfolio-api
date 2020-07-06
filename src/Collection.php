434d20526f96a8a38237f8f0ebadec2da7339704
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2020-07-06 01:24:59 +0100

    Reorder functions

--- src/Collection.php
+++ src/Collection.php
@@ -17,6 +17,37 @@ class Collection implements Arrayable, ArrayAccess, Countable, IteratorAggregate
         $this->count = count($items);
     }
 
+    protected function resetCount() {
+        $this->count = count($this->items);
+    }
+
+    public function setItem($key, $item) {
+        if ($key === null) {
+            $this->items[] = $item;
+        }
+        else {
+            $this->items[$key] = $item;
+        }
+        $this->resetCount();
+    }
+
+    public function addItem($item) {
+        $this->setItem(null, $item);
+    }
+
+    public function removeItem($key) {
+        unset($this->items[$key]);
+        $this->resetCount();
+    }
+
+    protected function doesItemExist($key) {
+        return array_key_exists($key, $this->items);
+    }
+
+    public function getItem($key) {
+        return $this->items[$key];
+    }
+
     public function getItems(): array {
         return $this->items;
     }
@@ -34,37 +65,6 @@ class Collection implements Arrayable, ArrayAccess, Countable, IteratorAggregate
         }
 
         return $array;
-    }
-
-    protected function resetCount() {
-        $this->count = count($this->items);
-    }
-
-    protected function doesItemExist($key) {
-        return array_key_exists($key, $this->items);
-    }
-
-    public function setItem($key, $item) {
-        if ($key === null) {
-            $this->items[] = $item;
-        }
-        else {
-            $this->items[$key] = $item;
-        }
-        $this->resetCount();
-    }
-
-    public function addItem($item) {
-        $this->setItem(null, $item);
-    }
-
-    public function getItem($key) {
-        return $this->items[$key];
-    }
-
-    public function removeItem($key) {
-        unset($this->items[$key]);
-        $this->resetCount();
     }
 
     // ArrayAccess //
