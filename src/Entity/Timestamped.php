e301f0fe217ad390842b59bae6919967a87fa84e
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2022-09-21 23:50:13 +0100

    Slightly rework timestamped trait to allow overriding core methods

--- src/Entity/Timestamped.php
+++ src/Entity/Timestamped.php
@@ -6,8 +6,24 @@ use DateTime;
 
 trait Timestamped {
 
-    public static function getDateTimeColumns(): array {
-        $columns = parent::getDateTimeColumns();
+    public function __construct() {
+        parent::__construct();
+
+        $this->addTimestampColumns();
+    }
+
+    protected function addTimestampColumns(): void {
+        if (!isset(static::$hasCreatedAt) || static::$hasCreatedAt) {
+            $this->setValue("created_at", null);
+        }
+
+        if (!isset(static::$hasUpdatedAt) || static::$hasUpdatedAt) {
+            $this->setValue("updated_at", null);
+        }
+    }
+
+    protected static function getTimestampColumns(): array {
+        $columns = [];
 
         if (!isset(static::$hasCreatedAt) || static::$hasCreatedAt) {
             $columns[] = "created_at";
@@ -20,19 +36,11 @@ trait Timestamped {
         return $columns;
     }
 
-    public function __construct() {
-        parent::__construct();
-
-        if (!isset(static::$hasCreatedAt) || static::$hasCreatedAt) {
-            $this->setValue("created_at", null);
-        }
-
-        if (!isset(static::$hasUpdatedAt) || static::$hasUpdatedAt) {
-            $this->setValue("updated_at", null);
-        }
+    public static function getDateTimeColumns(): array {
+        return array_merge(parent::getDateTimeColumns(), static::getTimestampColumns());
     }
 
-    public function save(): bool {
+    protected function setTimestamps(): void {
         $isNew = !$this->isLoaded();
 
         if ($isNew && (!isset(static::$hasCreatedAt) || static::$hasCreatedAt)) {
@@ -42,6 +50,10 @@ trait Timestamped {
         if (!isset(static::$hasUpdatedAt) || static::$hasUpdatedAt) {
             $this->setValue("updated_at", new DateTime());
         }
+    }
+
+    public function save(): bool {
+        $this->setTimestamps();
 
         return parent::save();
     }
