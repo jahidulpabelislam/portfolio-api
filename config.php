b61402927972b3318e945051601dbe556a7d7fee
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2022-11-15 23:21:36 +0000

    Remove debug config + remove comments

--- config.php
+++ config.php
@@ -1,16 +1,12 @@
 <?php
 
-$config->debug = false;
-
 $config->db_host = "overrideme";
 $config->db_name = "overrideme";
 $config->db_username = "overrideme";
 $config->db_password = "overrideme";
 
-// The secret key to use in Firebase's JWT
 $config->portfolio_admin_secret_key = "overrideme";
 
-// A list of other domains that can call this API
 $config->allowed_domains = [
     "jahidulpabelislam.com",
     "cms.jahidulpabelislam.com",
