9bfa50e4bd59210197491c7197d1647b339c874f
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2022-11-26 21:22:26 +0000

    Add migration to project type entity

---
+++ migrations/2023-09-28-222500-migrate-project-types.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Projects\Entity\Project;
+use App\Projects\Entity\Type;
+use JPI\Database\Query;
+
+require_once __DIR__ . "/../bootstrap.php";
+
+$totalCount = Project::newQuery()->where("type != ''")->count();
+
+$totalPages = ceil($totalCount / 10);
+
+$query = new Query\Builder(Project::getDatabase());
+$query->table(Project::getTable());
+
+for ($page = 1; $page <= $totalPages; $page++) {
+    $projects = Project::newQuery()
+        ->where("type != ''")
+        ->limit(10, $page)
+        ->select()
+    ;
+
+    foreach ($projects as $project) {
+        // Need to manually get the type column as not in the entity no more.
+        $typeName = (clone $query)
+            ->column("type")
+            ->where("id = {$project->getId()}")
+            ->limit(1)
+            ->select()
+            ["type"]
+        ;
+
+        $type = Type::getByNameOrCreate(trim($typeName));
+
+        $project->type_id = $type->getId();
+        $project->save();
+    }
+}
