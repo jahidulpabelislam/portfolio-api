1183499fea942cc719ef224299b7f21f917f2280
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2023-02-25 21:23:44 +0000

    Remove old method

--- src/Utils/Str.php
+++ src/Utils/Str.php
@@ -4,14 +4,6 @@ namespace App\Utils;
 
 class Str {
 
-    public static function toBool(?string $string, ?bool $default = false): ?bool {
-        if (in_array($string, ["", null], true)) {
-            return $default;
-        }
-
-        return filter_var($string, FILTER_VALIDATE_BOOLEAN);
-    }
-
     public static function machineToDisplay(string $value): string {
         $value = str_replace("_", " ", $value);
         return ucwords($value);
